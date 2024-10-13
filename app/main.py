01f3ce358e01398e901fc918985c173a03434a2a
Mauricio Oliveda <maurioliveda@gmail.com>
2024-10-13 16:57:06 -0300

    :construction: Register new routes.

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from contextlib import asynccontextmanager
 
 from .core import app_settings
 from .utils import create_tables
-from .api.v1.endpoints import auth_router, subscription_router
+from .api.v1.endpoints import auth_router, subscription_router, projects_router, tasks_router
 
 
 # Lifespan function to handle startup and shutdown events:
@@ -19,6 +19,9 @@ app = FastAPI(debug=app_settings.DEBUG, title=app_settings.APP_NAME,
               description=app_settings.APP_DESCRIPTION, version=app_settings.APP_VERSION, lifespan=lifespan)
 app.include_router(subscription_router,
                    prefix="/api/v1/subscription", tags=["Subscription"])
+app.include_router(
+    projects_router, prefix="/api/v1/projects", tags=["Projects"])
+app.include_router(tasks_router, prefix="/api/v1/tasks", tags=["tasks"])
 
 
 # Include/Register API routers
